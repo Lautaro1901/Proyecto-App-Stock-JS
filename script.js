8b9c4e412a50bb2417397dabc29a8b7f23a8eaf8
Lautaro Caraballo <lautaro.caraballo.1922@gmail.com>
2022-10-07 16:59:46 -0300

    inicio de proyecto

---
+++ script.js
@@ -0,0 +1,6 @@
+// const nombre = prompt ('Dame tu nombre perri'), 
+//     password = prompt ('Give me your pass     
+//     email = prompt ('give me your EMAIL');
+
+// alert('Bien ahi logeaste re cheto');
+// console.log (nombre + ' ' + password + ' ' + email)
